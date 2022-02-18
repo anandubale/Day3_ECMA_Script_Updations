2fd4d1916e85f42e0e7ad6e5a72e8c1606182e27
anandubale <au26199@gmail.com>
2022-02-18 13:20:12 +0530

    [Anand]- Added Features Introduce in ECMA Script 2016(ES7)

---
+++ ES-7_Features.js
@@ -0,0 +1,44 @@
+//1.array Prototype Include - done already
+
+//The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+
+// let numbers = [1, 2, 3, 4];
+
+// if(numbers.includes(2)) {  console.log('Array contains value');}
+
+
+//String.includes()
+var string_includes;
+
+//Method return true if present other wise false;
+// let text ="Hello worlds!"
+// console.log(text.includes("Hello"));  //true 
+// console.log(text.includes("hello"));  //false - case sentive
+
+
+
+
+
+//2.Exponential Operator:
+
+// let cubed = x => x ** 3;
+// cubed(2) // 8
+
+
+//Class Properties - Cover later in classes;
+
+// class Animal {
+//     constructor() {
+//         this.name = "Lion"
+//     }
+//     age = 0;
+// }
+
+// // That will be complied to:
+//     class Animal {
+//         constructor() {
+//             this.age = 0;
+//             this.name = "Lion";
+//         }
+//     }
